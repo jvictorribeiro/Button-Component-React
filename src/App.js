959607313580cb8a033a30cc349e3847fa10b80f
Joao Victor Ribeiro <jvictor.ribeiro03@gmail.com>
2021-04-04 15:02:39 -0300

    initial comit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './global.css';
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>Buttons</h1>
+
+        <button className="default">Default</button>
+
+        <button className="outline">Default</button>
+
+        <button className="text">Default</button>
+
+        <button className="disable-shadow">Default</button>
+
+        <button> </button>
+    </div>
+
+
+    )
+}
